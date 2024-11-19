73680620d79429576038f6968f6238f5578cba35
Kaveesh Khattar <kaveeshkhattar@gmail.com>
2024-11-19 12:38:39 +0530

    Updated UI for Complete Registration Card

--- src/app/additional-info/page.tsx
+++ src/app/additional-info/page.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import { Check, ChevronsUpDown } from "lucide-react";
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
 import {
   Command,
   CommandEmpty,
@@ -18,6 +17,26 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 interface Teacher {
   user: {
@@ -37,6 +56,14 @@ export default function UnSafePage() {
   const [teachers, setTeachers] = useState<Teacher[]>([]);
   const [open, setOpen] = useState<boolean>(false);
   const [value, setValue] = useState<string>("");
+
+  const handleSetRole = (value: string) => {
+    setRole(value);
+  };
+
+  const handleSetBoard = (value: string) => {
+    setBoard(value);
+  };
 
   useEffect(() => {
     // Fetch teacher data from the API
@@ -88,7 +115,175 @@ export default function UnSafePage() {
 
   return (
     <div className="p-4">
-      <div className="mt-4">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Add Additional Fields to Profile</CardTitle>
+          <CardDescription>
+            Make changes to your profile here. Click save when you&apos;re done.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="framework">Role</Label>
+              <Select onValueChange={handleSetRole}>
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Select a Role" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Roles</SelectLabel>
+                    <SelectItem value="STUDENT">Student</SelectItem>
+                    <SelectItem value="TEACHER">Teacher</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+
+            {role === "STUDENT" && (
+              <div className="grid w-full items-center gap-4 mt-4">
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="name">Grade</Label>
+                  <Input
+                    id="name"
+                    value={grade}
+                    onChange={(e) => setGrade(e.target.value)}
+                    placeholder="Grade"
+                  />
+                </div>
+
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="name">School</Label>
+                  <Input
+                    id="name"
+                    value={school}
+                    onChange={(e) => setSchool(e.target.value)}
+                    placeholder="School"
+                  />
+                </div>
+
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="framework">Board</Label>
+                  <Select onValueChange={handleSetBoard}>
+                    <SelectTrigger className="w-[180px]">
+                      <SelectValue placeholder="Select a Board" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectLabel>Boards</SelectLabel>
+                        <SelectItem value="CBSE">CBSE</SelectItem>
+                        <SelectItem value="ICSE">ICSE</SelectItem>
+                        <SelectItem value="IB">IB/IGCSE</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                </div>
+
+                <div className="mt-4">
+                  <Popover open={open} onOpenChange={setOpen}>
+                    <PopoverTrigger asChild>
+                      <Button
+                        variant="outline"
+                        role="combobox"
+                        aria-expanded={open}
+                        className="w-[200px] justify-between"
+                      >
+                        {value ? (
+                          <div className="flex items-center">
+                            <Image
+                              src={
+                                teachers.find(
+                                  (teacher) =>
+                                    teacher.user.id.toString() === value
+                                )?.user.imageUrl || ""
+                              }
+                              alt={
+                                teachers.find(
+                                  (teacher) =>
+                                    teacher.user.id.toString() === value
+                                )?.user.firstName || "Teacher"
+                              }
+                              width={20}
+                              height={20}
+                              className="mr-2"
+                            />
+                            <span>
+                              {
+                                teachers.find(
+                                  (teacher) =>
+                                    teacher.user.id.toString() === value
+                                )?.user.firstName
+                              }{" "}
+                              {
+                                teachers.find(
+                                  (teacher) =>
+                                    teacher.user.id.toString() === value
+                                )?.user.lastName
+                              }
+                            </span>
+                          </div>
+                        ) : (
+                          "Select teacher..."
+                        )}
+
+                        <ChevronsUpDown className="opacity-50" />
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-[250px] p-0">
+                      <Command>
+                        <CommandInput
+                          placeholder="Search teacher..."
+                          className="h-9"
+                        />
+                        <CommandList>
+                          <CommandEmpty>No teacher found.</CommandEmpty>
+                          <CommandGroup>
+                            {teachers.map((teacher) => (
+                              <CommandItem
+                                key={teacher.user.id}
+                                value={teacher.user.id.toString()}
+                                onSelect={(currentValue) => {
+                                  // Set the selected teacher's ID in the value state
+                                  setValue(
+                                    currentValue === value ? "" : currentValue
+                                  );
+                                  setOpen(false);
+                                }}
+                              >
+                                {/* Display teacher's name */}
+                                <Image
+                                  src={teacher.user.imageUrl}
+                                  alt={teacher.user.firstName}
+                                  width={20}
+                                  height={20}
+                                />
+                                {`${teacher.user.firstName} ${teacher.user.lastName}`}
+                                <Check
+                                  className={cn(
+                                    "ml-auto",
+                                    value === teacher.user.id.toString()
+                                      ? "opacity-100"
+                                      : "opacity-0"
+                                  )}
+                                />
+                              </CommandItem>
+                            ))}
+                          </CommandGroup>
+                        </CommandList>
+                      </Command>
+                    </PopoverContent>
+                  </Popover>
+                </div>
+              </div>
+            )}
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button onClick={handleUpdate}>Save Changes</Button>
+        </CardFooter>
+      </Card>
+
+      {/* <div className="mt-4">
         <select
           name="role"
           id="role"
@@ -99,10 +294,10 @@ export default function UnSafePage() {
           <option value="STUDENT">Student</option>
           <option value="TEACHER">Teacher</option>
         </select>
-      </div>
+      </div> */}
 
       {/* Additional Inputs for Students */}
-      {role === "STUDENT" && (
+      {/* {role === "STUDENT" && (
         <div className="mt-4 space-y-2">
           <input
             type="text"
@@ -167,9 +362,9 @@ export default function UnSafePage() {
                     </div>
                   ) : (
                     "Select a teacher..."
-                  )}
+                  )} */}
 
-                  <ChevronsUpDown className="opacity-50" />
+                  {/* <ChevronsUpDown className="opacity-50" />
                 </Button>
               </PopoverTrigger>
               <PopoverContent className="w-[250px] p-0">
@@ -192,9 +387,9 @@ export default function UnSafePage() {
                             );
                             setOpen(false);
                           }}
-                        >
+                        > */}
                           {/* Display teacher's name */}
-                          <Image
+                          {/* <Image
                             src={teacher.user.imageUrl}
                             alt={teacher.user.firstName}
                             width={20}
@@ -218,14 +413,14 @@ export default function UnSafePage() {
             </Popover>
           </div>
         </div>
-      )}
+      )} */}
 
-      <button
+      {/* <button
         onClick={handleUpdate}
         className="mt-4 bg-blue-500 text-white p-2 rounded-md"
       >
         Update User Info
-      </button>
+      </button> */}
     </div>
   );
 }
